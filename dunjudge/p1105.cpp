13aff253ee8f3f3035c3e2af74a0ad8255040939
Joey Teng <joey.teng.dev@gmail.com>
2017-01-23 23:58:30 +0800

    p1105 Washing AC
    Syntax, String

---
+++ dunjudge/p1105.cpp
@@ -0,0 +1,35 @@
+// washing
+
+#include<iostream>
+#include<string>
+
+int main(){
+    int S = 0;
+    int N = 0;
+
+    std::cin >> S >> N;
+
+    int count = S;
+
+    for (int i = 0; i < N; ++i){
+        std::string tmp;
+        std::getline(std::cin, tmp);
+        if (tmp == "USE"){
+            --count;
+        }
+        else if (tmp == "WASH"){
+            count = S; // reset
+        }
+        if (count < 0){
+            std::cout << "DIRTY" << std::endl;
+
+            return 0; // halt
+        }
+//        std::cout << tmp << ' ' << count << std::endl;
+    }
+
+    std::cout << "CLEAN" << std::endl;
+
+    return 0;
+}
+
