24f48eecb517f89278d4ec89b020f5946a7c4a7e
Joey Teng <joey.teng.dev@gmail.com>
2017-01-24 23:07:23 +0800

    p959 cablecar
    Trick, scan in both direction

--- dunjudge/p959.cpp
+++ dunjudge/p959.cpp
@@ -15,6 +15,7 @@ int main(){
     std::vector<int> h(int_cin, int_end);
 
     std::cout << std::max(scan(h.begin(), h.end()), scan(h.rbegin(), h.rend())) << std::endl; // Do it in both direction. See testcase self 4
+    // Do in both direction could cover all exceptional cases with simplest scanning process.
 
     return 0;
 }
@@ -23,9 +24,7 @@ template<typename T>
     int scan(T begin, T end){
 
     int max_length = 0;
-    int max_length_second_level = 0;
     int length = 0; // Merged length till now
-    int length_second_level = 0; // Sum of Possible length between two mountains lower than the heightest and cannot be merged yet.
     int length_extend = 0; // Possible length at the right of second high
     int height_left = 0; // First High
     int height_right = 0; //Second High
@@ -34,32 +33,15 @@ template<typename T>
         int height = *it;
 
         if (height >= height_left){ // First case. Reset all
-            // Wrong. Need to be modified.
-            // Exception: Leftest mountain is the highest && the rightest mountain (last mountain) is not the second highest one. 
-            // Solution: Record maximum height and second max, applying diff processing logic.
             height_left = height;
             height_right = 0;
-            // Total = | Merged | + | Sum of (not merged yet) | + | Extension |
-            max_length = std::max(std::max(max_length, length + length_second_level + length_extend + 1), max_length_second_level); // Important! +1 here to include the mountain at the right side! Do not +1 for all the cases if the rightest mountain is lower (in case that the mountain ends)
+            max_length = std::max(max_length, length + length_extend + 1); // Important! +1 here to include the mountain at the right side! Do not +1 for all the cases if the rightest mountain is lower (in case that the mountain ends)
             length = 0;
-            length_second_level = 0;
-            length_extend = 0;
-        } else if (height > height_right){ // Second case. Reset second level; Reset extension
-            height_right = height;
-            length += length_second_level + length_extend + 1; // Important also. Include this new mountain
-            max_length = std::max(max_length, length); // Update here
-            length_extend = 0;
-            length_second_level = 0;
-        } else if (height == height_right){ // Third case. Reset second level
-            max_length_second_level = std::max(length_second_level, length_extend + 1);
-            length_second_level += length_extend + 1;
             length_extend = 0;
         } else {
             ++length_extend;
         }
     }
-
-    max_length = std::max(max_length, max_length_second_level);
 
     return max_length;
 }
