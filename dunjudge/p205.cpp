4ea791d7be7b10b2bca7c6aed9924502584169fe
Joey Teng <joey.teng.dev@gmail.com>
2017-01-29 20:49:23 +0800

    p205 swappingcats
    Syntax

---
+++ dunjudge/p205.cpp
@@ -0,0 +1,28 @@
+// swapping cats
+
+#include<bits/stdc++.h>
+
+int main(){
+    int n = 0;
+    int s = 0;
+    std::vector<int> seat;
+
+    std::cin >> n >> s;
+    for (int i = 0; i < n; ++i){
+        seat.push_back(i);
+    }
+
+    for (int i = 0; i < s; ++i){
+        int x;
+        int y;
+
+        std::cin >> x >> y;
+        std::swap(seat.at(x), seat.at(y));
+    }
+
+    std::ostream_iterator<int> cout_int(std::cout, "\n");
+
+    std::copy(seat.begin(), seat.end(), cout_int);
+
+    return 0;
+}
