db0aa28b3b680c1847683c3e028b008561b56ebf
Joey Teng <joey.teng.dev@gmail.com>
2017-02-12 18:19:53 +0800

    visitfj USACO 2017Feb Gold Task 2 Timeout, haven't judged
    DP, possible

--- USACO/Gold/nocross.cpp
+++ USACO/Gold/nocross.cpp
@@ -11,7 +11,8 @@ const int N_MAX = 1000 + 10;
 int N = 0;
 int ans = 0;
 std::vector<int> breed;  // upp [0, N) low [N, 2N)
-int dp[N_MAX][2][2] = {0};  // i, upp/low, open/close
+std::vector<std::pair<int, int> > edges;
+std::vector<int> dp;
 
 int valid(int a, int b) {
     return (std::abs(a - b) <= 4);
@@ -22,28 +23,29 @@ int main() {
     std::istream_iterator<int> int_fin(fin);
     std::istream_iterator<int> int_end;
     breed.assign(int_fin, int_end);
+    dp.assign(N, 0);
 
-    if (valid(breed.at(0), breed.at(N))) {
-        dp[0][0][0] = 1;
-        dp[0][0][1] = 1;
-        dp[0][1][0] = 1;
-        dp[0][1][1] = 1;
-    }
-
-    for (int i = 1; i < N; ++i) {
-        dp[i][0][0] = std::max(dp[i - 1][0][0], dp[i - 1][0][1]);
-        dp[i][1][0] = std::max(dp[i - 1][1][0], dp[i - 1][1][1]);
-        for (int j = 0; j < i; ++j) {
-            if (valid(breed.at(i), breed.at(j + N))) {
-                dp[i][0][1] = std::max(dp[i][0][1], dp[j][1][0] + 1);
-            }
-            if (valid(breed.at(i + N), breed.at(j))) {
-                dp[i][1][1] = std::max(dp[i][1][1], dp[j][0][0] + 1);
+    for (int i = 0; i < N; ++i) {
+        for (int j = N - 1; j >= 0; --j) {
+            if (valid(breed.at(i), breed.at(N + j))) {
+                edges.push_back(std::make_pair(i, j));
             }
         }
-        std::cout << dp[i][0][0] << ' ' << dp[i][0][1] << ' ' << dp[i][1][0] << ' ' << dp[i][1][1] << std::endl;
     }
-    int ans = std::max(std::max(dp[N - 1][0][0], dp[N - 1][0][1]), std::max(dp[N - 1][1][0], dp[N - 1][1][1]));
+
+    // std::ostream_iterator<int> int_cout(std::cout, " ");
+    for (auto i : edges) {
+        // std::cout << i.first << ' ' << i.second << std::endl;
+        for (int j = i.second - 1; j >= 0; --j) {
+            dp[i.second] = std::max(dp[i.second], dp[j] + 1);
+        }
+        // std::copy(dp.begin(), dp.end(), int_cout);
+        // std::cout << std::endl;
+    }
+    int ans = 0;
+    for (auto i : dp) {
+        ans = std::max(ans, i);
+    }
 
     fout << ans << std::endl;
 
