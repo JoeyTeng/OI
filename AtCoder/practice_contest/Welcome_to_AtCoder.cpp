2bffe7c64667c5fff0ae9390ddd13fc36ef8ad25
Joey Teng <joey.teng.dev@gmail.com>
2017-02-17 13:43:16 +0800

    Welcome to AtCoder

---
+++ AtCoder/practice_contest/Welcome_to_AtCoder.cpp
@@ -0,0 +1,15 @@
+// Welcome to AtCoder
+
+#include<bits/stdc++.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    std::string s;
+
+    std::cin >> a >> b >> c >> s;
+    std::cout << (a + b + c) << ' ' << s << std::endl;
+
+    return 0;
+}
