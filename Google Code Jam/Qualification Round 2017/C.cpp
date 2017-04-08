00f421738e205569fbe6423e22afcb151389801d
Joey Teng <joey.teng.dev@gmail.com>
2017-04-08 22:59:23 +0800

    Google Code Jam 2017 Qualification Round C. Bathroom Stalls
    Incorrect 2
    Simulation, Math

--- Google Code Jam/Qualification Round 2017/C.cpp
+++ Google Code Jam/Qualification Round 2017/C.cpp
@@ -2,38 +2,6 @@
 
 #include<bits/stdc++.h>
 
-struct Pair {
-    int L;
-    int R;
-    int N;
-
-    Pair() {
-    }
-    Pair(int _L, int _R, int _N) {
-        L = std::min(_L, _R);
-        R = std::max(_L, _R);
-        N = _N;
-    }
-};
-
-struct HigherRank {
-    bool operator()(Pair& lhs, Pair& rhs) {
-        if (lhs.L < rhs.L) {
-            return 1;
-        } else if (lhs.L > rhs.L) {
-            return 0;
-        } else if (lhs.R < rhs.R) {
-            return 1;
-        } else if (lhs.R == rhs.R) {
-//            std::cout << "Merge " << lhs.L << ' ' << lhs.R << ' ' << lhs.N << ' ' << rhs.N << std::endl;
-            lhs.N += rhs.N;
-            rhs.N = 0;
-        }
-
-        return 0;
-    }
-};
-
 int main() {
     int T;
     std::cin >> T;
@@ -43,25 +11,27 @@ int main() {
         int K;
         std::cin >> N >> K;
 
-        std::priority_queue<Pair, std::vector<Pair>, HigherRank> Q;
-        Q.push(Pair(N >> 1, N - (N >> 1) - 1, 1));
-        for (int i = 1; i < K;) {
-            auto P = Q.top();
-            Q.pop();
-//            std::cout << "Loop " << i << ' ' << P.L << ' ' << P.R << ' ' << P.N << std::endl;
-            if (P.N == 0) {
-                continue;
-            }
-            auto A = (Pair(P.L >> 1, P.L - (P.L >> 1) - 1, P.N));
-            auto B = (Pair(P.R >> 1, P.R - (P.R >> 1) - 1, P.N));
-            Q.push(A);
-            Q.push(B);
-//            std::cout << "Push " << A.L << ' ' << A.R << ' ' << A.N << std::endl;
-//            std::cout << "Push " << B.L << ' ' << B.R << std::endl;
-            i += P.N << 1;
+        int64_t level = std::log2(K);
+        int64_t pre = std::pow(2, level) - 1;
+        int64_t rem = K - pre;
+        // a = n(x + 1) - m
+        int64_t n = std::pow(2, level);
+        int64_t x = (N - 1) / std::pow(2, level);
+        int64_t m = N - pre;
+        int64_t a = n * (x + 1) - m;
+
+        int64_t ans = 0;
+        if (n - a >= rem) {
+            ans = x + 1;
+        } else {
+            ans = x;
         }
-        auto P = Q.top();
-        std::cout << "Case #" << t + 1 << ": " << P.R << ' '  << P.L << std::endl;
+        --ans;
+
+//        std::cout << level << ' ' << pre << ' ' << rem << std::endl
+//                  << n << ' ' << x << ' ' << m << ' ' << a << ' ' << ans << std::endl;
+
+        std::cout << "Case #" << t + 1 << ": " << (ans - (ans >> 1)) << ' '  << (ans >> 1) << std::endl;
     }
 
     return 0;
