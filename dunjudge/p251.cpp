a5c7dfcaeca79e19f97af39879cc4d0a3040a97b
Joey Teng <joey.teng.dev@gmail.com>
2017-12-17 22:21:30 +0800

    p251 lights
    Mathematics

---
+++ dunjudge/p251.cpp
@@ -0,0 +1,16 @@
+// lights
+
+// Mathematics
+// a number will have odd number of factors if it is in the form of x^2, where
+// x\in \doubleZ
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::int64_t N;
+
+    std::cin >> N;
+    std::cout << std::int64_t(std::sqrt(N)) << std::endl;
+
+    return 0;
+}
