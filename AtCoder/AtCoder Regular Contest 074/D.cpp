940b2072d74fdf3d773e885fd021ed0626e10660
Joey Teng <joey.teng.dev@gmail.com>
2018-03-04 10:47:10 +0800

    AtCoder Regular Contest 074

---
+++ AtCoder/AtCoder Regular Contest 074/D.cpp
@@ -0,0 +1,12 @@
+/**
+ * @Author: Joey Teng <Toujour>
+ * @Date:   09-Jun-2017
+ * @Email:  joey.teng.dev@gmail.com
+ * @Filename: D.cpp
+ * @Last modified by:   Toujour
+ * @Last modified time: 09-Jun-2017
+ */
+
+
+
+#include<bits/stdc++.h>
