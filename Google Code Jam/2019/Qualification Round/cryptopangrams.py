7916141a373c6fe133c982939c12985e44ab34a1
Joey Teng <joey.teng.dev@gmail.com>
2019-04-10 22:40:13 +0800

    An update yet still WA... to deal with a corner case.

--- Google Code Jam/2019/Qualification Round/cryptopangrams.py
+++ Google Code Jam/2019/Qualification Round/cryptopangrams.py
@@ -2,7 +2,7 @@
 # @Email:  joey.teng.dev@gmail.com
 # @Filename: cryptopangrams.py
 # @Last modified by:   Joey Teng
-# @Last modified time: 06-Apr-2019
+# @Last modified time: 10-Apr-2019
 
 
 def gcd(a, b):
@@ -17,19 +17,37 @@ def main():
         n, l = [int(i) for i in input().split(' ')]
         digits = [int(i) for i in input().split(' ')]
         primes = set()
-        letter = []
+        letter = [0 for i in digits]
 
-        first = digits[0] // gcd(digits[0], digits[1])
-        letter.append(first)
-        primes.add(first)
-        for i in digits:
-            letter.append(i // letter[-1])
-            if (letter[-1] == 0):
-                print("ERROR")
-                return
-            primes.add(letter[-1])
+        start_index = -1
+        for index, i in enumerate(digits[:-1]):
+            key = gcd(i, digits[index + 1])
+            # print("gcd({}, {}) = {}".format(i, digits[index + 1], key))
+            if (key != i and key != digits[index + 1]):
+                # Prevent ABA-like patters.
+                # This would resulted in two same consecutive ciphertexts
+                start_index = index
+                letter[index] = digits[index] // key
+                letter[index + 1] = key
+                primes.add(letter[index])
+                primes.add(letter[index + 1])
+                # print("Key: {}".format(key))
+                break
 
-        print(sorted(primes))
+        # print("Start index: {}".format(start_index))
+        # print("letter: {}".format(letter))
+
+        for index in range(start_index - 1, -1, -1):
+            letter[index] = digits[index + 1] // letter[index + 1]
+            # print("letter[{}] = {}".format(index, letter[index]))
+            primes.add(digits[index] // letter[index + 1])
+
+        for index in range(start_index + 1, len(digits)):
+            letter[index] = digits[index - 1] // letter[index - 1]
+            # print("letter[{}] = {}".format(index, letter[index]))
+            primes.add(digits[index - 1] // letter[index - 1])
+
+        # print(sorted(primes))
         mapping = {}
         for index, i in enumerate(sorted(list(primes))):
             mapping[i] = chr(ord('A') + index)
