44d579042609dd0ac98243d033074d050ba747e4
Joey Teng <joey.teng.dev@gmail.com>
2019-10-19 14:22:09 +0100

    dunjudge p1477: archived

---
+++ dunjudge/p1477.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Author: Joey Teng
+ * @Email:  joey.teng.dev@gmail.com
+ * @Filename: p1477.cpp
+ * @Last modified by:   Joey Teng
+ * @Last modified time: 12-Mar-2018
+ */
+
+// spiraldance
+
+#include <bits/stdc++.h>
